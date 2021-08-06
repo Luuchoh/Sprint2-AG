1d28900888bd7fc12ee7001db3a2c35b8f14f9ac
Luuchoh <luxo997@gmail.com>
2021-08-06 15:17:50 -0500

    estructura

---
+++ src/pages/Statitics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Statitics = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Statitics
---
+++ src/routers/DailyBitsApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DailyBitsApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DailyBitsApp
