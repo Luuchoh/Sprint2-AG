8e274f44269bcef63831b2c684ed6ecafede050f
Woondeling Colina <josecolinamartinez@gmail.com>
2021-08-15 17:23:58 -0500

    subiendo profile

--- src/components/ProfileComp.jsx
+++ src/components/ProfileComp.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../style/style.css'
+import styled from 'styled-components';
+
+const Contenedor = styled.div`
+    width: 100%;
+    height: 88vh;
+    background: black;
+    color: white;
+    `
+
+    @media {}
+const Perfil = styled.div`
+    margin-top: 10%;
+    text-align: center;
+    display: grid;
+    justify-content:center;
+    `
+
+const H3 = styled.h3`
+margin: 0 auto;
+    text-align: center;
+    `
 
 const ProfileComp = () => {
     return (
-        <div>
-        <div className="contenedor">
-            <h3>Perfil</h3>
-            <div className="perfil">
+        
+        <Contenedor>
+            <H3>Perfil</H3>
+            <Perfil>
                 <img src="/" alt="" />
                 <h4>Francisco Javier</h4>
                 <p>francisco.javier@gmail.com</p>
-            </div>
+                
+                <Link to="/Login"><strong>Cerrar sesión</strong></Link>
+            </Perfil>
 
             <div>
-                <Link to="/Login"><strong>Cerrar sesión</strong></Link>
+                
             </div>
-        </div>
-        </div>
+        </Contenedor>
     )
 }
 
